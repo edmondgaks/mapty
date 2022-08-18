4782f3a4ce851a13cfda9609411e178eeb3a48d4
Edmond GAKS <edmondgaks@gmail.com>
2022-08-18 12:08:55 +0200

    using interfacesand exporting them to be used

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post,Put,Delete, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
   @Get()
   findAll(): string {
     return 'Get all items';
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import { Item } from './interfaces/item.interface';
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: "3434343545",
+      name: "Item 1",
+      qty: 100,
+      description: "This is item one"
+    },
+    {
+        id: "98494585",
+        name: "Item 2",
+        qty: 50,
+        description: "This is item two"
+      },
+  ];
+  findAll(): Item[] {
+    return this.items;
+  }
+}
